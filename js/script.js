ef15b86e825862297036fc23f4f849a7f49c3656
SiddharthBhardwaj3 <siddharthkumar538@gmail.com>
2024-01-12 13:37:59 +0530

    portfolio first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*================================== Typing Animation =================================*/
+
+var typed = new Typed(".typing",{
+
+    strings:["Freelancer","QA Automation Engineer"],
+    typeSpeed:60,
+    BackSpeed:60,
+    loop:true
+
+})
